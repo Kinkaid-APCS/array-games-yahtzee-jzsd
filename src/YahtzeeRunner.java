92e068cf63db56be3c4c99f7228c0bbd9649cb9e
Jason <jsh27335@gmail.com>
2023-10-25 12:41:38 -0500

    testing roll selected dice and the debugging methods

--- src/YahtzeeRunner.java
+++ src/YahtzeeRunner.java
@@ -4,12 +4,18 @@ public class YahtzeeRunner {
 	public static void main(String[] args) {
 
 		boolean testingMode = true;
+
+
 		Board test = new Board();
 		if (testingMode)
 		{
 			System.out.println("Start tests");
 			// enter test code here.
+			test.debugSetDice(1,1,1,1,1);
 			System.out.println(test);
+			test.rollSelectedDice("ABC");
+			System.out.println(test);
+
 			System.out.println("End tests");
 		}
 		else
